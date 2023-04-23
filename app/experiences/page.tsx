9f4616a1ec4a8942af3b8330c799e5ec4ccfe89d
ValentinyFilip <80767029+ValentinyFilip@users.noreply.github.com>
2023-04-23 21:05:37 +0200

    unused import

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -8,12 +8,12 @@ export default function About() {
             <main className="">
                 <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Experinces</h1>
                 <div>
-                    <h2 className="text-4xl font-bold dark:text-white">C</h2>
+                    <h2 className="text-4xl font-bold dark:text-white"><span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">C</span></h2>
                     <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">I have the most experience in C</p>
                     <Image src={C} alt="code v c" sizes="900px"></Image>
                 </div>
                 <div>
-                    <h2 className="text-4xl font-bold dark:text-white">RUST</h2>
+                    <h2 className="text-4xl font-bold dark:text-white"> <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">Rust</span></h2>
                     <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Rust is the language that i aspire to get job in</p>
                     <Image src={RUST} alt="code v rust" sizes="500px"></Image>
                 </div>
