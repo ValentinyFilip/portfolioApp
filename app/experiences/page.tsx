217b32e5d26eb1673cec6c2e2a96e4bf93b12faa
ValentinyFilip <80767029+ValentinyFilip@users.noreply.github.com>
2023-04-23 20:58:10 +0200

    some changes

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -8,9 +8,11 @@ export default function About() {
             <main className="">
                 <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Experinces</h1>
                 <div>
+                    <p>I have the most experience in C</p>
                     <Image src={C} alt="code v c" sizes="900px"></Image>
                 </div>
                 <div>
+                    <p>Rust is the language that i aspire to get job in</p>
                     <Image src={RUST} alt="code v rust" sizes="500px"></Image>
                 </div>
             </main>
